c30b110c3294c3af4d489e376ccf512b05058181
Ichilnia <44864669+Ichilnia@users.noreply.github.com>
2018-11-08 11:59:53 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -1,15 +1,16 @@
 #pragma once
 
-class Board
+class Board
 {
+	int boardWidth;
+	int boardHeight;
 protected:
-    bool **boardGame;
-    int boardWidth;
-    int boardHeight;
+	bool **boardGame;
 public:
-    Board() = default;
-    Board(int a, int b);
-    ~Board();
-    friend void write(Board a);
-    void changeValue(bool a,int x, int y);
+	Board() = default;
+	Board(int a, int b);
+	~Board();
+
+	void draw();
+	void changeValue(bool a,int x, int y);
 };
