ff7e1a43ae094ea8876f47cdaf325f97f70024d6
Ichilnia <44864669+Ichilnia@users.noreply.github.com>
2018-11-08 12:02:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,23 +4,6 @@
 
 using namespace std;
 
-void write(Board a)
-{
-    for (int j = 0; j < 100; j++)
-    {
-        for (int i = 0; i < 50; i++)
-        {
-            if (a.boardGame[i][j] == true)
-            {
-                cout << "W";
-            }
-            else cout << " ";
-        }
-
-        cout << endl;
-
-    }
-}
 
 int main()
 {
@@ -28,7 +11,7 @@ int main()
     Board g_board(500, 100);
     Snake g_snake(25, 50,g_board);
 
-    write(g_board);
+    g_board.draw();
     return 0;
 
 }
